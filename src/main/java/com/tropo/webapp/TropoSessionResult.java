58aaa3db10bd6b5c45f9c20bc6b7165fedb509f2
pratapat <pratapat@cisco.com>
2015-09-04 18:40:31 +0530

    merge tool

--- src/main/java/com/tropo/webapp/TropoSessionResult.java
+++ src/main/java/com/tropo/webapp/TropoSessionResult.java
@@ -1,5 +1,8 @@
 package com.tropo.webapp;
-
+/**
+*This is a result class, commented on local
+*this is just to learn merge tool
+*/
 import com.voxeo.tropo.TropoResult;
 
 public class TropoSessionResult {
